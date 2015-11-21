8d5f80ee855e9e2309eca7859ee578b1f7248228
Jent Zheng <jent.zheng@gmail.com>
2015-11-21 22:50:51 +0800

    update backup script and env

--- backup-all-db.sh
+++ backup-all-db.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-read -s -p "Mysql User: " USER
-echo "\n"
-read -s -p "Mysql Password: " PASS
-
-docker exec -it mysql mysqldump -u$USER -p$PASS --all-databases > ./backup/all-databases-$(date "+%b_%d_%Y_%H_%M_%S").sql
+docker exec -i mysql mysqldump -uroot -p --all-databases > ./backup/all-databases-$(date "+%b_%d_%Y_%H_%M_%S").sql
