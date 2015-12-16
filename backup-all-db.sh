ba458822b38dd4d8f77bdce78bd9fb8f639751fa
cxbig <i@cxbig.info>
2015-12-16 18:21:33 +0800

    update auto machine name sub-folder create

--- backup-all-db.sh
+++ backup-all-db.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker exec -i mysql mysqldump -uroot -p --all-databases | gzip -9 > ./backup/all-databases-$(date "+%b_%d_%Y_%H_%M_%S").sql.gz
+MACHINE_NAME=$(docker-machine active)
+mkdir -p ./backup/$MACHINE_NAME > /dev/null 2>&1
+docker exec -i mysql mysqldump -uroot -p --all-databases | gzip -9 > ./backup/$MACHINE_NAME/all-databases-$(date "+%b_%d_%Y_%H_%M_%S").sql.gz
