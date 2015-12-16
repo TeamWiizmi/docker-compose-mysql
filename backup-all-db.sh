c9a0b97ecc2204f545d7937f0b646bff4dd2c61b
cxbig <i@cxbig.info>
2015-12-16 18:02:28 +0800

    update sql command with gzip compress

--- backup-all-db.sh
+++ backup-all-db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -i mysql mysqldump -uroot -p --all-databases > ./backup/all-databases-$(date "+%b_%d_%Y_%H_%M_%S").sql
+docker exec -i mysql mysqldump -uroot -p --all-databases | gzip -9 > ./backup/all-databases-$(date "+%b_%d_%Y_%H_%M_%S").sql.gz
